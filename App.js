a0fa4c16c7aea86a4c8f4feda7bd42e24faef3d6
MahirKh <mkhandaker1999@gmail.com>
2021-05-23 16:24:49 -0400

    Added CustomerHome Text

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import CustomerHome from './Components/CustomerHome/CustomerHome'
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>HELLO 2:51</Text>
+      <CustomerHome/>
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/CustomerHome/CustomerHome.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default function CustomerHome() {
+    return (
+        <View>
+            <Text>Customer Home</Text>
+        </View>
+    );
+}
