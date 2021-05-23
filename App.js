5fbcf8ed6f724fc5480bdc6c519ffa3f841487e0
MahirKh <mkhandaker1999@gmail.com>
2021-05-23 14:38:29 -0400

    Mahir's Commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>pullup</Text>
+      <Text>PullUp!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
