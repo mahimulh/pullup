5e8d565b7ebef0b758be8b400bd521bf6155918a
mahimulh13 <mahimulh13@gmail.com>
2021-08-03 21:45:10 -0400

    file structure

--- App.js
+++ App.js
@@ -1,22 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import CustomerHome from './Components/CustomerHome/CustomerHome'
-import { StyleSheet, Text, View } from 'react-native';
+// In App.js in a new project
 
-export default function App() {
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+//routing attempt --need to delete
+function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <CustomerHome/>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.navigate('Details')}
+      />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Overview' }}/>
+        <Stack.Screen name="Details" component={DetailsScreen}  options={{ title: 'Overview' }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ sharedComponents/cards/cards.js
@@ -0,0 +1,2 @@
+//either one card type or multiple ones for different pages
+//these are the listing cards
